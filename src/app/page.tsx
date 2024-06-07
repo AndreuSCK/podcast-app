60cb4442663de2f2c5e9ecb094130225a5eaf1ae
AndreuSCK <andreu.sck@gmail.com>
2024-06-07 14:06:56 +0200

    Working home page - TopPodcasts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import TopPodcasts from "./TopPodcasts";
+import { PodcastType } from "./_types/types";
+import getTopPodcasts from "./_utils/getTopPodcasts";
 import styles from "./page.module.css";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
+  const podcasts: PodcastType[] = await getTopPodcasts();
+
   return (
     <main className={styles.main}>
-      
+      <Suspense fallback={<p>Loading...</p>}>
+        <TopPodcasts podcastsData={podcasts}/>
+      </Suspense>
     </main>
   );
 }
