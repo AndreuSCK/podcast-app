8e6e12446a7cb07b4c4f3b4452d44dfef6c1ebf1
AndreuSCK <andreu.sck@gmail.com>
2024-06-12 12:50:26 +0200

    Refactor usePodcast hook and fetchEpisodes function

--- src/app/_utils/usePodcast.ts
+++ src/app/_utils/usePodcast.ts
@@ -17,41 +17,42 @@ const usePodcast = (id: string) => {
   useEffect(() => {
     if (isPodcastLoading) return;
     if (podcastData && currentEpisode === id) return;
-    fetchEpisodes();
-  }, [podcastData]);
 
-  const fetchEpisodes = async () => {
-    try {
-      const response = await fetch(
-        `${ALLORIGINSURL}${encodeURIComponent(
-          `https://itunes.apple.com/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`
-        )}`,
-        {
-          method: "GET",
-          next: {
-            revalidate: 24 * 60 * 60, // revalidate after 24 hours
-          },
+    const fetchEpisodes = async () => {
+      setIsPodcastLoading(true);
+      try {
+        const response = await fetch(
+          `${ALLORIGINSURL}${encodeURIComponent(
+            `https://itunes.apple.com/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`
+          )}`,
+          {
+            method: "GET",
+            next: {
+              revalidate: 24 * 60 * 60, // revalidate after 24 hours
+            },
+          }
+        );
+        const data = await response.json();
+        const content = await JSON.parse(data.contents);
+        if (content && content.resultCount === 0) {
+          throw new Error();
         }
-      );
-      const data = await response.json();
-      const content = await JSON.parse(data.contents);
-      if (content && content.resultCount === 0) {
-        throw new Error();
+        setCurrentEpisode(id);
+        const podcastData = {
+          info: content.results.shift(),
+          episodes: content.results,
+          podcastCount: content.resultCount - 1,
+        };
+        setPodcastData(podcastData);
+      } catch (error) {
+        console.log(error);
+        setError(error as Error);
+      } finally {
+        setIsPodcastLoading(false);
       }
-      setCurrentEpisode(id);
-      const podcastData = {
-        info: content.results.shift(),
-        episodes: content.results,
-        podcastCount: content.resultCount - 1,
-      };
-      setPodcastData(podcastData);
-    } catch (error) {
-      console.log(error);
-      setError(error as Error);
-    } finally {
-      setIsPodcastLoading(false);
-    }
-  };
+    };
+    fetchEpisodes();
+  }, [id]);
 
   const getEpisode = (episodeId: string) => {
     return podcastData?.episodes?.find(
@@ -64,7 +65,6 @@ const usePodcast = (id: string) => {
     error,
     getEpisode,
     podcastData,
-    fetchEpisodes,
     currentEpisode,
   };
 };
